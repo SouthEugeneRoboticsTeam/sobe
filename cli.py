7c261b7cdbdee6d9fe4304f41089e1de012f76ce
Jackson <st.japa6@gmail.com>
2017-11-28 17:11:18 -0800

    Separate CLI parsing from main process (#12)

---
+++ cli.py
@@ -0,0 +1,93 @@
+# Parses input from command line arguments
+import argparse
+import json
+
+
+class ArgState:  # Anonymous class used by the returned state
+    pass
+
+
+def parse_predict():
+    # This factors in command line arguments and config and merges them to
+    # produce a single configuration object
+    argparser = argparse.ArgumentParser(
+        description='Train and validate YOLO_v2 model on any dataset')
+
+    argparser.add_argument(
+        '-c',
+        '--config',
+        help='path to configuration file')
+
+    argparser.add_argument(
+        '-w',
+        '--weights',
+        help='path to pretrained weights')
+
+    argparser.add_argument(
+        '-i',
+        '--input',
+        help='path to an image or an video (mp4 format)')
+
+    args = argparser.parse_args()
+
+    config_path = args.conf
+    with open(config_path) as config_buffer:
+        config = json.load(config_buffer)
+
+    argstate = ArgState
+    argstate.architecture = config['model']['architecture']
+    argstate.input_size = config['model']['input_size']
+    argstate.labels = config['model']['labels']
+    argstate.max_box_per_image = config['model']['max_box_per_image']
+    argstate.anchors = config['model']['anchors']
+
+    if args.weights is not None:
+        argstate.weights = args.weights
+    else:
+        argstate.weights = config['train']['saved_weights_name']
+
+    argstate.input = args.input
+
+    return argstate
+
+
+def parse_train():
+    argparser = argparse.ArgumentParser(
+        description='Train and validate YOLO_v2 model on any dataset')
+
+    argparser.add_argument(
+        '-c',
+        '--config',
+        help='path to configuration file')
+
+    args = argparser.parse_args()
+
+    config_path = args.conf
+    with open(config_path) as config_buffer:
+        config = json.loads(config_buffer.read())
+
+    argstate = ArgState
+    argstate.train.annot_folder = config['train']['train_annot_folder']
+    argstate.train.image_folder = config['train']['train_image_folder']
+    argstate.labels = config['model']['labels']
+    argstate.valid.annot_folder = config['valid']['valid_annot_folder']
+    argstate.valid.image_folder = config['valid']['valid_image_folder']
+    argstate.architecture = config['model']['architecture']
+    argstate.input_size = config['model']['input_size']
+    argstate.mbpi = config['model']['max_box_per_image']
+    argstate.anchors = config['model']['anchors']
+    argstate.train.times = config['train']['train_times']
+    argstate.valid.times = config['valid']['valid_times']
+    argstate.nb_epoch = config['train']['nb_epoch']
+    argstate.learning_rate = config['train']['learning_rate']
+    argstate.batch_size = config['train']['batch_size']
+    argstate.warmup_bs = config['train']['warmup_batches']
+    argstate.object_scale = config['train']['object_scale']
+    argstate.no_object_scale = config['train']['no_object_scale']
+    argstate.coord_scale = config['train']['coord_scale']
+    argstate.class_scale = config['train']['class_scale']
+    argstate.saved_weights_name = config['train']['saved_weights_name']
+    argstate.debug = config['train']['debug']
+    argstate.pretrained_weights = config['train']['pretrained_weights']
+
+    return argstate
--- predict.py
+++ predict.py
@@ -1,55 +1,31 @@
 #! /usr/bin/env python
 
-import argparse
+import cli
 import os
 import cv2
 import numpy as np
 from tqdm import tqdm
-from preprocessing import parse_annotation
 from utils import draw_boxes
 from frontend import YOLO
-import json
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-argparser = argparse.ArgumentParser(
-    description='Train and validate YOLO_v2 model on any dataset')
 
-argparser.add_argument(
-    '-c',
-    '--conf',
-    help='path to configuration file')
+def main(argstate):
 
-argparser.add_argument(
-    '-w',
-    '--weights',
-    help='path to pretrained weights')
-
-argparser.add_argument(
-    '-i',
-    '--input',
-    help='path to an image or an video (mp4 format)')
-
-
-def _main_(args):
-
-    config_path = args.conf
-    weights_path = args.weights
-    image_path = args.input
-
-    with open(config_path) as config_buffer:
-        config = json.load(config_buffer)
+    weights_path = argstate.weights
+    image_path = argstate.input
 
     ###############################
     #   Make the model
     ###############################
 
-    yolo = YOLO(architecture=config['model']['architecture'],
-                input_size=config['model']['input_size'],
-                labels=config['model']['labels'],
-                max_box_per_image=config['model']['max_box_per_image'],
-                anchors=config['model']['anchors'])
+    yolo = YOLO(architecture=argstate.architecture,
+                input_size=argstate.input_size,
+                labels=argstate.labels,
+                max_box_per_image=argstate.max_box_per_image,
+                anchors=argstate.anchors)
 
     ###############################
     #   Load trained weights
@@ -85,7 +61,7 @@ def _main_(args):
             _, image = video_reader.read()
 
             boxes = yolo.predict(image)
-            image = draw_boxes(image, boxes, config['model']['labels'])
+            image = draw_boxes(image, boxes, argstate.labels)
 
             video_writer.write(np.uint8(image))
 
@@ -94,7 +70,7 @@ def _main_(args):
     else:
         image = cv2.imread(image_path)
         boxes = yolo.predict(image)
-        image = draw_boxes(image, boxes, config['model']['labels'])
+        image = draw_boxes(image, boxes, argstate.labels)
 
         print(len(boxes), 'boxes are found')
 
@@ -102,5 +78,5 @@ def _main_(args):
 
 
 if __name__ == '__main__':
-    args = argparser.parse_args()
-    _main_(args)
+    argstate = cli.parse_predict()
+    main(argstate)
